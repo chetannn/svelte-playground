ab2d5a83876c91198384b1b6dd5dbc057937667e
Chetan <chetankharel7@gmail.com>
2024-02-13 22:41:26 +0545

    feat: register action (#7)

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -7,29 +7,31 @@
 
 <div class="flex min-h-screen items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
 	<div class="w-full max-w-md space-y-8">
-		<Card.Root>
-			<Card.Header class="space-y-1">
-				<Card.Title class="text-2xl">Create a new account</Card.Title>
-			</Card.Header>
-			<Card.Content class="grid gap-4">
-				<div class="grid gap-2">
-					<Label for="email">Email</Label>
-					<Input id="email" type="email" placeholder="m@example.com" />
-				</div>
-				<div class="grid gap-2">
-					<Label for="password">Password</Label>
-					<Input id="password" type="password" />
-				</div>
+		<form method="post" action="?/register">
+			<Card.Root>
+				<Card.Header class="space-y-1">
+					<Card.Title class="text-2xl">Create a new account</Card.Title>
+				</Card.Header>
+				<Card.Content class="grid gap-4">
+					<div class="grid gap-2">
+						<Label for="email">Email</Label>
+						<Input id="email" type="email" placeholder="m@example.com" />
+					</div>
+					<div class="grid gap-2">
+						<Label for="password">Password</Label>
+						<Input id="password" type="password" />
+					</div>
 
-				<div class="grid gap-2">
-					<Label for="confirm_password">Confirm Password</Label>
-					<Input id="confirm_password" type="password" />
-				</div>
-			</Card.Content>
-			<Card.Footer>
-				<Button class="w-full">Register</Button>
-			</Card.Footer>
-		</Card.Root>
+					<div class="grid gap-2">
+						<Label for="confirm_password">Confirm Password</Label>
+						<Input id="confirm_password" type="password" />
+					</div>
+				</Card.Content>
+				<Card.Footer>
+					<Button type="submit" class="w-full">Register</Button>
+				</Card.Footer>
+			</Card.Root>
+		</form>
 
 		<p class="px-8 text-center text-sm text-muted-foreground">
 			Already have an account ?
