444854f3255462c0c7456e030622d42604ceea06
chetannn <chetankharel7@gmail.com>
2024-02-18 09:39:47 +0545

    feat: add lucide icons

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -3,6 +3,8 @@
 	import { goto } from '$app/navigation';
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
 	import * as Avatar from '$lib/components/ui/avatar';
+	import { LayoutDashboard, Settings, MessageCircleMore, Users } from 'lucide-svelte';
+
 	import { cn } from '$lib/utils';
 	import type { PageData } from './$types';
 	export let data: PageData;
@@ -18,78 +20,23 @@
 						<div class="space-y-1">
 							<a href="/dashboard">
 								<Button variant="ghost" class="w-full justify-start">
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										viewBox="0 0 24 24"
-										fill="none"
-										stroke="currentColor"
-										stroke-width="2"
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										class="mr-2 h-4 w-4"
-									>
-										<rect width="7" height="7" x="3" y="3" rx="1" />
-										<rect width="7" height="7" x="14" y="3" rx="1" />
-										<rect width="7" height="7" x="14" y="14" rx="1" />
-										<rect width="7" height="7" x="3" y="14" rx="1" />
-									</svg>
+									<LayoutDashboard class="mr-2 h-4 w-4" />
 									Dashboard
 								</Button>
 							</a>
 
 							<a href="/dashboard/users">
 								<Button variant="secondary" class="w-full justify-start">
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										viewBox="0 0 24 24"
-										fill="none"
-										stroke="currentColor"
-										stroke-width="2"
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										class="mr-2 h-4 w-4"
-									>
-										<circle cx="12" cy="12" r="10" />
-										<polygon points="10 8 16 12 10 16 10 8" />
-									</svg>
+									<Users class="mr-2 h-4 w-4" />
 									Users
 								</Button>
 							</a>
 							<Button variant="ghost" class="w-full justify-start">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									viewBox="0 0 24 24"
-									fill="none"
-									stroke="currentColor"
-									stroke-width="2"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									class="mr-2 h-4 w-4"
-								>
-									<rect width="7" height="7" x="3" y="3" rx="1" />
-									<rect width="7" height="7" x="14" y="3" rx="1" />
-									<rect width="7" height="7" x="14" y="14" rx="1" />
-									<rect width="7" height="7" x="3" y="14" rx="1" />
-								</svg>
+								<MessageCircleMore class="mr-2 h-4 w-4" />
 								Messages
 							</Button>
 							<Button variant="ghost" class="w-full justify-start">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									viewBox="0 0 24 24"
-									fill="none"
-									stroke="currentColor"
-									stroke-width="2"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									class="mr-2 h-4 w-4"
-								>
-									<path d="M4.9 19.1C1 15.2 1 8.8 4.9 4.9" />
-									<path d="M7.8 16.2c-2.3-2.3-2.3-6.1 0-8.5" />
-									<circle cx="12" cy="12" r="2" />
-									<path d="M16.2 7.8c2.3 2.3 2.3 6.1 0 8.5" />
-									<path d="M19.1 4.9C23 8.8 23 15.1 19.1 19" />
-								</svg>
+								<Settings class="mr-2 h-4 w-4" />
 								Settings
 							</Button>
 						</div>
@@ -111,7 +58,7 @@
 								</Avatar.Root>
 							</Button>
 						</DropdownMenu.Trigger>
-						<DropdownMenu.Content class="w-56">
+						<DropdownMenu.Content class="w-52">
 							<DropdownMenu.Label class="font-normal">
 								<div class="flex flex-col space-y-1">
 									<p class="text-sm font-medium leading-none">
@@ -121,25 +68,12 @@
 							</DropdownMenu.Label>
 							<DropdownMenu.Separator />
 							<DropdownMenu.Group>
-								<DropdownMenu.Item on:click={() => goto('/profile')}>
-									Profile
-									<DropdownMenu.Shortcut>⇧⌘P</DropdownMenu.Shortcut>
-								</DropdownMenu.Item>
-								<DropdownMenu.Item on:click={() => goto('/login')}>
-									Billing
-									<DropdownMenu.Shortcut>⌘B</DropdownMenu.Shortcut>
-								</DropdownMenu.Item>
-								<DropdownMenu.Item on:click={() => goto('/settings')}>
-									Settings
-									<DropdownMenu.Shortcut>⌘S</DropdownMenu.Shortcut>
-								</DropdownMenu.Item>
+								<DropdownMenu.Item on:click={() => goto('/profile')}>Profile</DropdownMenu.Item>
+								<DropdownMenu.Item on:click={() => goto('/settings')}>Settings</DropdownMenu.Item>
 							</DropdownMenu.Group>
 							<DropdownMenu.Separator />
 
-							<DropdownMenu.Item on:click={() => goto('/login')}>
-								Log out
-								<DropdownMenu.Shortcut>⇧⌘Q</DropdownMenu.Shortcut>
-							</DropdownMenu.Item>
+							<DropdownMenu.Item on:click={() => goto('/login')}>Log out</DropdownMenu.Item>
 						</DropdownMenu.Content>
 					</DropdownMenu.Root>
 				</div>
